fcddf9205b70560134e98d892812e5d2446aa16c
antonio <ajoaoff@gmail.com>
2018-04-26 11:24:24 -0300

    Check if there is a way back. When discovering circuits, if a circuit from A to B exists, check if there is a path from B to A. If such a path does not exist, send an alert.

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 """Utility functions to be used in this Napp"""
 
+from kytos.core import log
+from kytos.core import KytosEvent, KytosNApp
+
 TRANSLATE_NAMES = {
     'dl_src': 'eth_src',
     'dl_dst': 'eth_dst',
@@ -59,24 +62,65 @@ def format_result(trace):
                       'in_port': step['in']['port'],
                       'out_port': step['out']['port']}
         if 'vlan' in step['out']:
-            new_result.update({'vlan': step['out']['vlan']})
+            new_result.update({'out_vlan': step['out']['vlan']})
+        if 'vlan' in step['in']:
+            new_result.update({'in_vlan': step['in']['vlan']})
         result.append(new_result)
     return result
 
-def clean_circuits(circuits):
+
+def clean_circuits(circuits, controller):
     """Remove sub-circuits."""
     cleaned_circuits = []
+    event = KytosEvent(name='amlight/kytos_courier.slack_send')
+    content = {
+        'channel': 'tests',
+        'source': 'amlight/sdntrace_cp'
+    }
     for circuit in circuits:
+        sub = False
         for other in circuits:
-            if circuit == other:
+            if circuit['circuit'] == other['circuit']:
                 continue
             sub = True
-            for step in circuit:
-                if step not in other:
+            for step in circuit['circuit']:
+                if step not in other['circuit']:
                     sub = False
                     break
             if sub:
                 break
         if not sub:
             cleaned_circuits.append(circuit)
+
+    for circuit in cleaned_circuits:
+        has_return = False
+        for other in cleaned_circuits:
+            if compare_endpoints(circuit['circuit'][0],
+                                 other['circuit'][-1]) \
+                    and compare_endpoints(circuit['circuit'][-1],
+                                          other['circuit'][0]):
+                has_return = True
+        if not has_return:
+            content['m_body'] = \
+                'Circuit %s has no way back' % circuit['circuit']
+            event.content['message'] = content
+            controller.buffers.app.put(event)
     return cleaned_circuits
+
+
+def compare_endpoints(endpoint1, endpoint2):
+    if endpoint1['dpid'] != endpoint2['dpid']:
+        return False
+    if endpoint1['in_port'] != endpoint2['out_port']:
+        return False
+    if 'in_vlan' in endpoint1 and 'out_vlan' in endpoint2:
+        if endpoint1['in_vlan'] != endpoint2['out_vlan']:
+            return False
+    elif 'in_vlan' in endpoint1 or 'out_vlan' in endpoint2:
+        return False
+    if 'out_vlan' in endpoint1 and 'in_vlan' in endpoint2:
+        if endpoint1['out_vlan'] != endpoint2['in_vlan']:
+            return False
+    elif 'out_vlan' in endpoint1 or 'in_vlan' in endpoint2:
+        return False
+    return True
