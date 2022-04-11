2bb4149eba55025125f22a5320d7066e61f98b76
rmotitsuki <rogerio.motitsuki@gmail.com>
2022-04-11 13:42:20 -0300

    project configuration for python 3.9

--- setup.py
+++ setup.py
@@ -177,10 +177,10 @@ class CITest(TestCommand):
 
     def run(self):
         """Run unit tests with coverage, doc tests and linter."""
-        coverage_cmd = f"python3.6 setup.py coverage {0}".format(
+        coverage_cmd = f"python3.9 setup.py coverage {0}".format(
             self.get_args()
         )
-        lint_cmd = "python3.6 setup.py lint"
+        lint_cmd = "python3.9 setup.py lint"
         cmd = f"{0} && {1}".format(coverage_cmd, lint_cmd)
         check_call(cmd, shell=True)
 
@@ -335,6 +335,6 @@ setup(name=f'{NAPP_USERNAME}_{NAPP_NAME}',
       classifiers=[
           'License :: OSI Approved :: MIT License',
           'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.9',
           'Topic :: System :: Networking',
       ])
